49cf101f23882dcea6e645b14dc52dba666ce2e4
Alexey Koptyaev <alexey.koptyaev@auriga.com>
2016-12-23 17:47:10 +0300

    Week 3 Task 1,2 finished (Changing Money, Maximizing the Value of a Loot (Fractional Knapsack))

--- AlgorithmicToolbox/Program.cs
+++ AlgorithmicToolbox/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AlgorithmicToolbox
 {
@@ -12,15 +8,74 @@ namespace AlgorithmicToolbox
         static void Main(string[] args)
         {
             var input = Console.ReadLine();
-            var _args = input.Split(' ').Select(x => Int64.Parse(x)).ToArray();
+            var _args = input.Split(' ').Select(x => Int32.Parse(x)).ToArray();
 
-            Console.WriteLine(Week2.PartialFibSumLast(_args[0], _args[1]));
+            Console.WriteLine(Lib.LootBag(_args[0], _args[1]));
             Console.ReadLine();
         }
     }
 
     class Lib
     {
+        public static int ChangingMoney(int m)
+        {
+            int[] nominals = new[] { 10, 5, 1 };
+            int result = 0;
 
+            foreach (var nominal in nominals)
+            {
+                result += m / nominal;
+                m %= nominal;
+            }
+
+            return result;
+        }
+
+        public static double LootBag(int n, int W)
+        {
+            int[] w = new int[n];
+            int[] v = new int[n];
+            double[] a = new double[n];
+            var V = new double();
+            var A = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine();
+                var args = input.Split(' ').Select(x => int.Parse(x)).ToArray();
+
+                var ai = (double)args[0] / args[1];
+
+                int j;
+                for (j = 0; j < i; j++)
+                {
+                    if (ai > a[j])
+                    {
+                        Array.Copy(a, j, a, j + 1, n - j - 1);
+                        Array.Copy(w, j, w, j + 1, n - j - 1);
+                        Array.Copy(v, j, v, j + 1, n - j - 1);
+                        break;
+                    }
+                }
+                a[j] = ai;
+                v[j] = args[0];
+                w[j] = args[1];
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if (W == 0)
+                {
+                    return V;
+                }
+                var x = Math.Min(w[i], W);
+                V += x * a[i];
+                w[i] -= x;
+                A[i] += x;
+                W -= x;
+            }
+
+            return V;
+        }
     }
 }
---
+++ AlgorithmicToolbox/Week3.cs
@@ -0,0 +1,19 @@
+﻿namespace AlgorithmicToolbox
+{
+    class Week3
+    {
+        public static int ChangingMoney(int m)
+        {
+            int[] nominals = new[] { 10, 5, 1 };
+            int result = 0;
+
+            foreach (var nominal in nominals)
+            {
+                result += m / nominal;
+                m %= nominal;
+            }
+
+            return result;
+        }
+    }
+}

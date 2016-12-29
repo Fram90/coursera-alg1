08ca51243729f60482e2cee4a8ed9f3ec3d801c0
Alexey Koptyaev <alexey.koptyaev@auriga.com>
2016-12-29 14:43:13 +0300

    Week 4 Task 3 (Improving Quick Sort) Finished

--- AlgorithmicToolbox/Program.cs
+++ AlgorithmicToolbox/Program.cs
@@ -7,11 +7,15 @@ namespace AlgorithmicToolbox
     {
         static void Main(string[] args)
         {
+
+
             var input = Console.ReadLine();
+            input = Console.ReadLine();
             var _args = input.Split(' ').Select(x => Int32.Parse(x)).ToArray();
 
             Lib.QuickSort(_args, 0, _args.Length - 1);
             Console.WriteLine(String.Join(" ", _args));
+
 
             Console.ReadLine();
         }
@@ -19,45 +23,6 @@ namespace AlgorithmicToolbox
 
     class Lib
     {
-        public static void QuickSort(int[] a, int l, int r)
-        {
-            if (l >= r)
-            {
-                return;
-            }
-
-            var m = Partition3(a, l, r);
-            QuickSort(a, l, m - 1);
-            QuickSort(a, m + 1, r);
-
-            //var k = new Random().Next(l, r);
-            //Swap(ref a[l], ref a[k]);
-
-
-        }
-
-        static int Partition3(int[] a, int l, int r)
-        {
-            int j = l;
-
-            for (int i = l + 1; i <= r; i++)
-            {
-                if (a[i] <= a[l])
-                {
-                    j++;
-                    Swap(ref a[i], ref a[j]);
-                }
-            }
-            Swap(ref a[l], ref a[j]);
-
-            return j;
-        }
-
-        static void Swap(ref int foo, ref int bar)
-        {
-            int tmp = foo;
-            foo = bar;
-            bar = tmp;
-        }
+        
     }
 }
--- AlgorithmicToolbox/Week4.cs
+++ AlgorithmicToolbox/Week4.cs
@@ -43,5 +43,55 @@ namespace AlgorithmicToolbox
 
             return -1;
         }
+
+        public static void QuickSort(int[] a, int l, int r)
+        {
+            if (l >= r)
+            {
+                return;
+            }
+
+            var k = new Random().Next(l, r);
+            Swap(ref a[l], ref a[k]);
+
+            var m = Partition3(a, l, r);
+            QuickSort(a, l, m[0] - 1);
+            QuickSort(a, m[1] + 1, r);
+        }
+
+        static int[] Partition3(int[] a, int l, int r)
+        {
+            var x = a[l];
+            int j = l;
+            int i = 0;
+
+            for (int k = l + 1; k <= r; k++)
+            {
+                if (a[k] < x)
+                {
+                    j++;
+                    Swap(ref a[k], ref a[j]);
+                    if (i > 0)
+                    {
+                        Swap(ref a[k], ref a[j + i]);
+                    }
+                }
+                else if (a[k] == x)
+                {
+                    i++;
+                    Swap(ref a[k], ref a[j + i]);
+                }
+            }
+            Swap(ref a[l], ref a[j]);
+
+            return new int[] { j, j + i };
+        }
+
+        static void Swap(ref int foo, ref int bar)
+        {
+            int tmp = foo;
+            foo = bar;
+            bar = tmp;
+        }
     }
 }
